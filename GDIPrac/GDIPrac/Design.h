7c6ce027746c7ebcf902b3ba0e03d3cb853e2df2
susie <susie_su94@hotmail.com>
2014-07-18 23:57:49 -0400

    Added an icon, bgm, character&action structs(temporary)

---
+++ GDIPrac/GDIPrac/Design.h
@@ -0,0 +1,22 @@
+#include<Windows.h>
+
+struct CHARACTER
+{
+	int CurrHp;    //Current health points
+	int MaxHp;     //Max health points
+	int CurrMp;    //Current magic points
+	int Level;     //Level of the character
+	int Strength;  //Strength of the character
+	int Agility;   //Agility of the character
+	int Intellect; //Intellect of the character
+};
+
+
+enum Actions
+{
+	ACTION_NORMAL = 0,	 //normal attack
+	ACTION_CRITICAL = 1, //critival strike
+	ACTION_MAGIC = 2,    //magic attack
+	ACTION_MISS = 3,
+	ACTION_RECOVER = 4
+};
